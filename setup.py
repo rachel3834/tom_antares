48ba9c8cff8d9fdffbd821bcc1124e6c9ad54c8e
David Collom <dcollom@lco.global>
2020-10-06 07:50:38 -0700

    Got stream working

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tom-antares',
-    version='0.1.0',
+    version='1.0.1',
     description='Antares broker module for the TOM Toolkit',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- tom_antares/antares.py
+++ tom_antares/antares.py
@@ -1,10 +1,10 @@
-from datetime import datetime, timezone
 import logging
 import requests
 
 from antares_client import StreamingClient
 from antares_client.exceptions import AntaresException
 from antares_client.search import get_by_ztf_object_id
+from astropy.time import Time, TimezoneInfo
 from django import forms
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -30,10 +30,11 @@ def get_available_streams(url=ANTARES_TAG_URL):
 
 def get_stream_choices():
     streams = get_available_streams()
-    return [(s['id'], s['id']) for s in streams]
+    return [(s['id'] + '_staging', s['id']) for s in streams]
 
 
 class AntaresBrokerForm(GenericQueryForm):
+    # TODO: allow multiple selection of streams
     stream = forms.ChoiceField(choices=get_stream_choices)
 
 
@@ -48,32 +49,42 @@ class AntaresBroker(GenericBroker):
                 'api_key': antares_creds['api_key'],
                 'api_secret': antares_creds['api_secret']
             }
-            if antares_creds.get('group'):
+            if antares_creds.get('group'):  # Group should only be set if it exists, otherwise Antares manages it
                 self.config['group'] = antares_creds['group']
 
         except KeyError:
             raise ImproperlyConfigured('Missing ANTARES API credentials')
 
     def fetch_alerts(self, parameters):
-        print(parameters)
+        print(f'parameters: {parameters}')
         stream = parameters['stream']
         client = StreamingClient([stream], **self.config)
-        alert_stream = client.iter()
+        alert_stream = client.iter(20)
         alerts = []
-        while len(alerts) < 20:
+        # TODO: Add timeout in case there aren't alerts
+        while len(alerts) < 5:  # TODO: change this back to 20
             try:
                 alert = next(alert_stream)
             except (AntaresException, marshmallow.exceptions.ValidationError):
                 break
+            serialized_alert = {
+                'locus_id': alert[1].locus_id,
+                'ra': alert[1].ra,
+                'dec': alert[1].dec,
+                'properties': alert[1].properties,
+                'tags': alert[1].tags,
+                'lightcurve': alert[1].lightcurve.to_json(),
+                'catalogs': alert[1].catalogs,
+                'alerts': [{
+                    'alert_id': alert.alert_id,
+                    'mjd': alert.mjd,
+                    'properties': alert.properties} for alert in alert[1].alerts]
+            }
+            alert = (alert[0], serialized_alert)
             alerts.append(alert)
         return iter(alerts)
 
     def fetch_alert(self, id):
-        # Antares doesn't have programmatic access to alerts, so we use MARS here
-        # url = 'https://mars.lco.global/?format=json&candid={}'.format(id)
-        # response = requests.get(url)
-        # response.raise_for_status()
-        # return response.json()['results'][0]
         alert = get_by_ztf_object_id(id)
         return alert
 
@@ -82,44 +93,30 @@ class AntaresBroker(GenericBroker):
 
     def to_target(self, alert):
         _, alert = alert
-        print(alert)
         target = Target.objects.create(
-            identifier=alert.locus_id,
-            name=alert.properties['ztf_object_id'],
+            identifier=alert['locus_id'],
+            name=alert['properties']['ztf_object_id'],
             type='SIDEREAL',
-            ra=alert.ra,
-            dec=alert.dec,
+            ra=alert['ra'],
+            dec=alert['dec'],
         )
-        if alert.properties.get('horizons_targetname'):
-            TargetName.objects.create(target=target, name=alert.properties.get('horizons_targetname'))
+        if alert['properties'].get('horizons_targetname'):
+            TargetName.objects.create(target=target, name=alert['properties'].get('horizons_targetname'))
         return target
 
     def to_generic_alert(self, alert):
-        # print(alert)
-        # _, alert = alert
-        # print(alert)
-        # url = 'https://antares.noao.edu/loci/{}'.format(alert['new_alert']['alert_id'])
-        # timestamp = datetime.utcfromtimestamp(alert['timestamp_unix']).replace(tzinfo=timezone.utc)
-        # return GenericAlert(
-        #     timestamp=timestamp,
-        #     url=url,
-        #     id=alert['new_alert']['properties']['ztf_candid'],
-        #     name=alert['new_alert']['properties']['ztf_object_id'],
-        #     ra=alert['new_alert']['ra'],
-        #     dec=alert['new_alert']['dec'],
-        #     mag=alert['new_alert']['properties']['ztf_magpsf'],
-        #     score=alert['new_alert']['properties']['ztf_rb']
-        # )
         _, alert = alert
-        url = f'{ANTARES_BASE_URL}/loci/{alert.locus_id}'
-        timestamp = datetime.utcfromtimestamp(alert.newest_alert_observation_time).replace(tzinfo=timezone.utc)
+        url = f"{ANTARES_BASE_URL}/loci/{alert['locus_id']}"
+        timestamp = Time(
+            alert['properties'].get('newest_alert_observation_time'), format='mjd', scale='utc'
+        ).to_datetime(timezone=TimezoneInfo())
         return GenericAlert(
             timestamp=timestamp,
             url=url,
-            id=alert.locus_id,
-            name=alert.properties['ztf_object_id'],
-            ra=alert.ra,
-            dec=alert.dec,
-            mag=alert.properties.get('newest_alert_magnitude', ''),
-            score=alert.alerts[-1].properties.get('ztf_rb', '')
+            id=alert['locus_id'],
+            name=alert['properties']['ztf_object_id'],
+            ra=alert['ra'],
+            dec=alert['dec'],
+            mag=alert['properties'].get('newest_alert_magnitude', ''),
+            score=alert['alerts'][-1]['properties'].get('ztf_rb', '')
         )
