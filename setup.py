b81563448a474c7a85a776b9e939a2029d5b1f7d
David Collom <dcollom@lco.global>
2020-10-12 17:47:06 -0700

    Updating version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tom-antares',
-    version='1.0.0',
+    version='1.0.2',
     description='Antares broker module for the TOM Toolkit',
     long_description=long_description,
     long_description_content_type='text/markdown',
