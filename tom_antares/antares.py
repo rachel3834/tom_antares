f53681f827242a6aa9ed3cd97cce39de921d6fa5
David Collom <dcollom@lco.global>
2021-08-12 14:44:37 -0700

    Refactored elasticsearch query construction to avoid issues with not-required fields

--- tom_antares/antares.py
+++ tom_antares/antares.py
@@ -306,18 +306,18 @@ class ANTARESBroker(GenericBroker):
         }
 
     def fetch_alerts(self, parameters: dict) -> iter:
-        tags = parameters['tag']
-        nobs_gt = parameters['nobs__gt']
-        nobs_lt = parameters['nobs__lt']
-        sra = parameters['ra']
-        sdec = parameters['dec']
-        ssr = parameters['sr']
-        mjd_gt = parameters['mjd__gt']
-        mjd_lt = parameters['mjd__lt']
-        mag_min = parameters['mag__min']
-        mag_max = parameters['mag__max']
-        elsquery = parameters['esquery']
-        ztfid = parameters['ztfid']
+        tags = parameters.get('tag')
+        nobs_gt = parameters.get('nobs__gt')
+        nobs_lt = parameters.get('nobs__lt')
+        sra = parameters.get('ra')
+        sdec = parameters.get('dec')
+        ssr = parameters.get('sr')
+        mjd_gt = parameters.get('mjd__gt')
+        mjd_lt = parameters.get('mjd__lt')
+        mag_min = parameters.get('mag__min')
+        mag_max = parameters.get('mag__max')
+        elsquery = parameters.get('esquery')
+        ztfid = parameters.get('ztfid')
         if ztfid:
             query = {
                 "query": {
@@ -335,65 +335,51 @@ class ANTARESBroker(GenericBroker):
         elif elsquery:
             query = elsquery
         else:
-            query = {
+            filters = []
+        
+        if nobs_gt or nobs_lt:
+            nobs_range = {'range': {'properties.num_mag_values': {}}}
+            if nobs_gt:
+                nobs_range['range']['properties.num_mag_values']['gte'] = nobs_gt
+            if nobs_lt:
+                nobs_range['range']['properties.num_mag_values']['lte'] = nobs_lt
+            filters.append(nobs_range)
+        
+        if mjd_lt:
+            mjd_lt_range = {'range': {'properties.newest_alert_observation_time': {'lte': mjd_lt}}}
+            filters.append(mjd_lt_range)
+        
+        if mjd_gt:
+            mjd_gt_range = {'range': {'properties.oldest_alert_observation_time': {'gte': mjd_gt}}}
+            filters.append(mjd_gt_range)
+
+        if mag_min or mag_max:
+            mag_range = {'range': {'properties.newest_alert_magnitude': {}}}
+            if mag_min:
+                mag_range['range']['properties.newest_alert_magnitude']['gte'] = mag_min
+            if mag_max:
+                mag_range['range']['properties.newest_alert_magnitude']['lte'] = mag_max
+            filters.append(mag_range)
+
+        if sra and ssr:  # TODO: add cross-field validation
+            ra_range = {'range': {'ra': {'gte': sra-ssr, 'lte': sra+ssr}}}
+            filters.append(ra_range)
+
+        if sdec and ssr:  # TODO: add cross-field validation
+            dec_range = {'range': {'dec': {'gte': sdec-ssr, 'lte': sdec+ssr}}}
+            filters.append(dec_range)
+
+        if tags:
+            filters.append({'terms': {'tags': tags}})
+
+        query = {
                 "query": {
                     "bool": {
-                        "filter": [
-                            {
-                                "range": {
-                                    "properties.num_mag_values": {
-                                        "gte": nobs_gt,
-                                        "lte": nobs_lt,
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "properties.newest_alert_observation_time": {
-                                        "lte": mjd_lt,
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "properties.oldest_alert_observation_time": {
-                                        "gte": mjd_gt,
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "properties.newest_alert_magnitude": {
-                                        "gte": mag_min,
-                                        "lte": mag_max,
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "ra": {
-                                        "gte": sra-ssr,
-                                        "lte": sra+ssr,
-                                    }
-                                }
-                            },
-                            {
-                                "range": {
-                                    "dec": {
-                                        "gte": sdec-ssr,
-                                        "lte": sdec+ssr,
-                                    }
-                                }
-                            },
-                            {
-                                "terms": {
-                                    "tags": tags
-                                }
-                            }
-                        ]
+                        "filter": filters
                     }
                 }
             }
+
         loci = antares_client.search.search(query)
 #        if ztfid:
 #            loci = get_by_ztf_object_id(ztfid)
